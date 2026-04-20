1561a9b9f62ecd09961ca111f4d5a4352d5bbc60
apekshagulhane2001-sudo <apekshagulhane2001@gmail.com>
2026-04-20 22:21:35 +0530

    Casting class implemented

---
+++ src/com/java/core/Casting.java
@@ -0,0 +1,15 @@
+package com.java.core;
+
+public class Casting {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 3;
+		
+		double mul = a*b;
+        System.out.println(mul); 
+		
+		
+		
+	}
+
+}
