67b4479d2fc3af555b0a23f4c3e96059afd1f0a4
apekshagulhane2001-sudo <apekshagulhane2001@gmail.com>
2026-04-20 21:54:16 +0530

    Datatype emlimanted

---
+++ src/com/java/core/DataType.java
@@ -0,0 +1,15 @@
+package com.java.core;
+
+public class DataType {
+	public static void main (String[] args) {
+int rollNumber = 6754;
+		
+		System.out.println("rollNumber");
+		System.out.println(rollNumber);
+		
+		double percentage = 96.12;
+		System.out.println("percentage");
+		System.out.println(percentage);
+	
+	} 
+}
--- src/com/java/core/HelloWorld.java
+++ src/com/java/core/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.java.core;
 
-public class HelloWorld {
+public class HelloWorld{
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		
 	}
 
 }
+
+
