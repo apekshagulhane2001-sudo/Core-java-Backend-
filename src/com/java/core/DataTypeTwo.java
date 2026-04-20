4c1da437538870c45ceb3000d3a68f5ac19cb87c
apekshagulhane2001-sudo <apekshagulhane2001@gmail.com>
2026-04-20 22:15:52 +0530

    DataTypeTwo class implemented

---
+++ src/com/java/core/DataTypeTwo.java
@@ -0,0 +1,52 @@
+package com.java.core;
+
+public class DataTypeTwo {
+	public static void main(String[] args) {
+	//whole number
+			int rollNumber = 85;
+			System.out.println("rollNumber");
+			System.out.println(rollNumber);
+			
+			short age = 95;
+			System.out.println("short : age");
+			System.out.println(age);
+			
+			long phoneNumber = 1234567890;
+			System.out.println("long : phoneNumber");
+			System.out.println(phoneNumber);
+			
+			byte a = 23;
+			System.out.println("Byte : a");
+			System.out.println(a);
+			
+			//Decimal
+			
+			double percentage = 98;
+			System.out.println("Double : percentage");
+			System.out.println(percentage);
+			
+			float average = 23.65f;
+			System.out.println("Float : average");
+			System.out.println(average);
+			
+			//Character - only alphabets A-Z = 26
+			
+			
+			char alpha = 'A';
+			System.out.println("Character : alpha");
+			System.out.println(alpha);
+			
+			//Boolean
+			boolean isGraduate = true;
+			System.out.println("Boolean : isGraduate");
+			System.out.println(isGraduate);
+			
+			
+			//String -> non-primitive datatype -> words, statements and alpha-numeric
+			
+			String st = "Today is Friday..";
+			System.out.println(st);
+	}
+
+
+}
