fd27c7f2e95b2d07c41f3f2da7ddedf2cffdd36a
apekshagulhane2001-sudo <apekshagulhane2001@gmail.com>
2026-04-23 00:15:54 +0530

    String Scanner

---
+++ src/com/java/core/TestScanner/ScannerString.java
@@ -0,0 +1,14 @@
+package com.java.core.TestScanner;
+import java.util.Scanner;
+
+public class ScannerString {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input");
+		String ip = sc.nextLine();
+		System.out.println("op is -> "+ip);
+		
+	}
+
+}
