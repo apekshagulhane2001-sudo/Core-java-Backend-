5a1c97333fd2131ce8d9c8e614fe8ef685a08017
apekshagulhane2001-sudo <apekshagulhane2001@gmail.com>
2026-04-24 20:31:33 +0530

    conditional if statement

---
+++ src/com/java/core/conditionalstatement/ifConditionTest.java
@@ -0,0 +1,17 @@
+package com.java.core.conditionalstatement;
+
+public class ifConditionTest {
+	public static void main(String[] args) {
+		
+		int a = 13;
+		if(a % 2 ==0) {
+			System.out.println("It is a even number");
+			
+		}
+		else {
+		System.out.println("It is a odd number");	
+			
+		}
+	}
+
+}
---
+++ src/com/java/core/conditionalstatement/nestedifcondition.java
@@ -0,0 +1,23 @@
+
+package com.java.core.conditionalstatement;
+
+public class nestedifcondition {
+	public static void main(String[] args) {
+		
+	
+	int a = 12;
+	//condition -> if number is divisble by 2 and 3 then is it a good number
+	if(a%2 ==0) {
+		
+	System.out.println("number is divisible by 2");
+	
+	if (a%3 ==0) {
+		System.out.println("it is a good number");
+		
+		
+	}
+		}
+	
+	
+	}
+}
